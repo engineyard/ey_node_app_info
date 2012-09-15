df86e9f31d65b822a4d4b942d7a8567415306d81
Jim Lindley <jim@jimlindley.com>
2012-09-14 23:29:02 -0700

    Cleaned up calling convention.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-ey_node_package_info
+ey_node_app_info
 ====================
 
 Parses a Node.js app's package.json file to figure out which node version and commands to use.
@@ -9,80 +9,17 @@ EY Node Package Info
 
 Internal tool to extract information about a Node.js application.
 
-
-
-
-
-
 Examples:
 ---
 
+    ey_node_app_info engine-version --app /path/to/app
+    # output like '0.8.7'
 
-ey_node_package_info --package-json ./test/fixtures/missing_engine.json
-ey_node_package_info # assumes --package-json of ./package.json
+    ey_node_app_info command start --app /path/to/app
+    # output like 'node ./app.js' or 'npm start'
 
+    ey_node_app_info command start --app /path/to/app
+    # output like 'node ./app.js' or 'npm start'
 
-
-Given:
-
-{
-  "scripts": {
-    "start": "node ./bin/http-server",
-    "test": "vows --spec --isolate",
-    "predeploy": "echo This will be run before deploying the app",
-    "postdeploy": "echo This will be run after deploying the app"
-    "migrate": "echo migratey stuff"
-  },
-  "main": "./bin/web-server",
-  "engines" {
-    "node": ">=0.6"
-  }
-}
-
-Output:
-
-
-ey-node-package-info --engine-install # installs or fails w/ log stuff
-ey-node-package-info --engine-path # "/opt/nodejs/0.8.7/bin"
-ey-node-package-info --command start # "node ./bin/web-server"
-ey-node-package-info --command predeploy # "..."
-ey-node-package-info --command postdeploy # "..."
-ey-node-package-info --command migrate # "..."
-
-ey-node-package-info # with no args writes out node_app.env
-
-
-Given:
-
-no package.json file
-
-Output:
-
-EXPLODE
-
-
-Given:
-
-package.json with no engine
-
-Outputs:
-
-ey-node-package-info --engine-path # whatever eselected node there is main recipes: 0.8.7
-
-Given:
-
-package.json with no scripts
-
-predeploy: no-op
-postdeploy: no-op
-migrate: no-op
-start: look for {app,web,server}.{js,coffee} # app.js
-
-
-
-Version support:
-
-only 0.8.x or 0.8.7
-
-/>=0.6
+    
 
---
+++ test/app_info_test.js
@@ -0,0 +1,156 @@
+var vows = require('vows'),
+  assert = require('assert'),
+  expect = require('chai').expect,
+  sys = require('sys'),
+  exec = require('child_process').exec;
+
+var packageInfoExecutable = "./bin/ey_node_app_info";
+
+var invokeCommand = function(cmdString, optionString, callback){
+  var cmd = [packageInfoExecutable, cmdString, optionString].join(" ");
+  if(process.env.DEBUG) console.log('Invoking: ', cmd, '\n');
+  exec(cmd, callback);
+};
+
+vows.describe("integrated testing").addBatch({
+  'help': {
+    'when given: --help': {
+      topic: function() { invokeCommand('--help', '', this.callback) },
+      "it exits with return code 1": function(error, stdout, stderr){
+        assert.equal(error.code, 1);
+      },
+      "there is no stderr output": function(error, stdout, stderr){
+        assert.equal('', stderr);
+      },
+      "there is help on stdout": function(error, stdout, stderr){
+        expect(stdout).to.include("Available options");
+      }
+    },
+  },
+  'with available versions': {
+    topic: '--available ./test/sample-apps/available-node-versions.json',
+    'a basic application': {
+      topic: function(available){
+        return '--app ./test/sample-apps/basic '+available;
+      },
+      "when given: engine-version": {
+        topic: function(optionString) {
+          invokeCommand('engine-version', optionString, this.callback);
+        },
+        "it should return 0.8.7": function(error, stdout, stderr){
+          assert.equal("0.8.7\n", stdout);
+        }
+      }
+    },
+    'an outdated package.json (version to old to be available)': {
+      topic: function(available){
+        return '--app ./test/sample-apps/outdated ' + available;
+      },
+      'when asked for engine-version': {
+        topic: function(optionString) {
+          invokeCommand('engine-version', optionString, this.callback);
+        },
+        "it should blow up with an error message": function(error, stdout, stderr){
+          assert.equal(error.code, 1);
+          expect(stderr).to.include("Could not find").include("0.4.0");
+        }
+      }
+    },
+    'unspecified engine in package.json': {
+      topic: function(available) {
+        return '--app ./test/sample-apps/minimal '+ available;
+      },
+      'when asked for engine-version': {
+        topic: function(optionString) {
+          invokeCommand('engine-version', optionString, this.callback);
+        },
+        "it returns the default version": function(error, stdout, stderr){
+          assert.equal(error, null);
+          expect(stdout).to.equal("1337.0.0\n");
+        }
+      }
+    },
+    'querying basic commands': {
+      topic: function(available){
+        return '--app ./test/sample-apps/basic ' + available;
+      },
+      'start command': {
+        topic: function(optionString) {
+          invokeCommand('command start', optionString, this.callback);
+        },
+        "returns 'npm start' if that key is present": function(error, stdout, stderr){
+          assert.equal(error, null);
+          expect(stdout).to.equal("npm run-script start\n");
+        }
+      },
+      'predeploy command': {
+        topic: function(optionString) {
+          invokeCommand('command predeploy', optionString, this.callback);
+        },
+        "returns 'npm run-script predeploy' if that key is present": function(error, stdout, stderr){
+          assert.equal(error, null);
+          expect(stdout).to.equal("npm run-script predeploy\n");
+        }
+      },
+      'postdeploy command': {
+        topic: function(optionString) {
+          invokeCommand('command postdeploy', optionString, this.callback);
+        },
+        "returns 'npm run-script postdeploy' if that key is present": function(error, stdout, stderr){
+          assert.equal(error, null);
+          expect(stdout).to.equal("npm run-script postdeploy\n");
+        }
+      },
+    },
+    'querying available commands (results when not specified)': {
+      topic: function(available){
+        return ' --app ./test/sample-apps/minimal ' + available;
+      },
+      'start command': {
+        topic: function(optionString) {
+          invokeCommand('command start', optionString, this.callback);
+        },
+        "fallback to app.js": function(error, stdout, stderr){
+          assert.equal(error, null);
+          expect(stdout).to.include("node ./app.js");
+        }
+      },
+      'predeploy command not specified': {
+        topic: function(optionString) {
+          invokeCommand('command predeploy', optionString, this.callback);
+        },
+        "returns nothing since that key is not present": function(error, stdout, stderr){
+          assert.equal(error.code, 1);
+          expect(stdout).to.equal("");
+        }
+      },
+      'postdeploy command not specified': {
+        topic: function(optionString) {
+          invokeCommand('command postdeploy', optionString, this.callback);
+        },
+        "returns nothing since that key is not present": function(error, stdout, stderr){
+          assert.equal(error.code, 1);
+          expect(stdout).to.equal("");
+        }
+      },
+    },
+    'insufficient information to start application': {
+      topic: function(available){
+        return ' --app ./test/sample-apps/wont_start ' + available;
+      },
+      'start command': {
+        topic: function(optionString) {
+          invokeCommand('command start', optionString, this.callback);
+        },
+        "exist non-zero if command is not present": function(error, stdout, stderr){
+          assert.equal(error.code, 1);
+          expect(stderr).to.include('No `npm start` specified.')
+        }
+      },
+    },
+
+  }
+
+
+}).run();
+
